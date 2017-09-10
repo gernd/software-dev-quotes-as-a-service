c7855c8a96fdd44dc3c3ae5c7732bd1c8ed4ddc7
Gernot Pointner <gernot.pointner@googlemail.com>
2017-09-10 15:53:22 +0200

    Use Spring Boot 1.5.6

---
+++ src/main/java/io/github/gernd/sqaas/SqaasApplication.java
@@ -0,0 +1,12 @@
+package io.github.gernd.sqaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqaasApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/gernd/sqaas/SqaasApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.gernd.sqaas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SqaasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
